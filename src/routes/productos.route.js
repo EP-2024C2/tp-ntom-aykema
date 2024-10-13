34bb571b149cc700de6dd27648c760c25c34c12c
kevincaria <kevindcaria@gmail.com>
2024-10-13 16:33:39 -0300

    se agregan endpoints para productos/fabricantes y productos/componentes

--- src/routes/productos.route.js
+++ src/routes/productos.route.js
@@ -13,4 +13,13 @@ route.delete('/productos/:id', productosController.deleteProductoById)
 
 route.put('/productos/:id', productosController.updateProductoById)
 
+route.get('/productos/:id/componentes', productosController.getProductoYSusComponentes)
+
+route.get('/productos/:id/fabricantes', productosController.getProductoYSusFabricantes)
+
+route.post('/productos/:id/componentes', productosController.addComponenteToProducto)
+
+route.post('/productos/:id/fabricantes', productosController.addFabricanteToProducto)
+
+
 module.exports = route
