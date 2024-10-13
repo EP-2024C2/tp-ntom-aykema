27435d64c83b6e58eab04177b33eace12fd2af5a
Martin Bruno <mbruno@desarrollosocial.gob.ar>
2024-10-13 18:27:22 -0300

    se agrego validateId

--- src/middlewares/generic.middleware.js
+++ src/middlewares/generic.middleware.js
@@ -4,6 +4,18 @@ const requestTime = (req, _ , next) => {
     console.log({ url: req.url, method: req.method , fechaHora: new Date() })
     next()
 }
-middleware.requestTime = requestTime
 
-module.exports = middleware
+const validateId = (Model) => {
+    return async (req, res, next) => {
+      const id = req.params.id
+      const instance = await Model.findByPk(id)
+  
+      if (!instance) {
+        return res.status(404).json({ mensaje: `${Model.name} con id ${id} no encontrado` })
+      }
+  
+      next()
+    }
+}
+
+module.exports = { requestTime, validateId}
--- src/routes/componentes.route.js
+++ src/routes/componentes.route.js
@@ -2,19 +2,20 @@ const { Router } = require('express')
 const componentesController = require('../controllers/componentes.controller')
 const middlewares = require('../middlewares')
 const { componentesSchema } = require('../schemas')
+const { Componente } = require('../models')
 
 const route = Router()
 
 route.get('/componentes', componentesController.getAllComponentes)
 
-route.get('/componentes/:id', componentesController.getComponenteById)
+route.get('/componentes/:id', middlewares.genericMiddleware.validateId(Componente), componentesController.getComponenteById)
 
 route.post('/componentes', middlewares.schemaValidator(componentesSchema), componentesController.createComponente)
 
-route.delete('/componentes/:id', componentesController.deleteComponenteById)
+route.delete('/componentes/:id', middlewares.genericMiddleware.validateId(Componente), componentesController.deleteComponenteById)
 
-route.put('/componentes/:id', middlewares.schemaValidator(componentesSchema), componentesController.updateComponenteById)
+route.put('/componentes/:id', middlewares.schemaValidator(componentesSchema), middlewares.genericMiddleware.validateId(Componente), componentesController.updateComponenteById)
 
-route.get('/componentes/:id/productos', componentesController.getComponenteYSusProductos)
+route.get('/componentes/:id/productos', middlewares.genericMiddleware.validateId(Componente), componentesController.getComponenteYSusProductos)
 
 module.exports = route
